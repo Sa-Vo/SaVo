cff7d166bf7c576f79b1d07cd83f26673c8aad7b
vova.msn <vova.moskalyuk11jr@gmail.com>
2020-09-21 22:42:45 +0300

    responsive and fixed bag

--- src/js/text.js
+++ src/js/text.js
@@ -1,1 +1,19 @@
+// const text = baffle(".text-pizda");
+// text.set({
+//   characters: "░▓▓ <█▒█▓ ░░/▓▓ ░/█ ///██ ▒░█▒ <▒█ ▓█░▓ /█▒▓",
+//   speed: 200,
+// });
+// text.start();
+// text.reveal(4000);
+const text = baffle(".text-pizda");
+var i = 1;
 
+do {
+  text.set({
+    characters: "░▓▓ <█▒█▓ ░░/▓▓ ░/█ ///██ ▒░█▒ <▒█ ▓█░▓ /█▒▓",
+    speed: 200,
+  });
+  text.start();
+  text.reveal(4000);
+  i++;
+} while (i < 100);
